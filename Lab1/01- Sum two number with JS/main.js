22a73785755ab2384a5387f0df626fe72f868d94
Hussein Elmlah <147069168+hussein-elmlah@users.noreply.github.com>
2023-12-15 19:31:20 +0200

    Delete Lab1 directory

--- Lab1/01- Sum two number with JS/main.js
+++
@@ -1,5 +0,0 @@
-
-var x = Number(prompt("Enter first Number"));
-var y = Number(prompt("Enter second Number"));
-var sum = x+y;
-console.log(`result of sum ${x} and ${y} = ${sum}`);
--- Lab1/04- Age Calculator/main.js
+++
@@ -1,8 +0,0 @@
-var userAgeInYears = Number(prompt(`How old are you?`));
-var userAgeInDays = userAgeInYears * 365.25;
-
-if (userAgeInYears > 0) {
-    alert(`${userAgeInYears} years is roughly ${userAgeInDays} days`);
-} else {
-  alert(`invalid iput. Try again.`);
-}
