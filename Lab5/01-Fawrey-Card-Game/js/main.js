48c736338c7411487deecac9ff895f48418da7fa
Hussein Elmlah <147069168+hussein-elmlah@users.noreply.github.com>
2023-12-19 19:36:42 +0200

    Update main.js

--- Lab5/01-Fawrey-Card-Game/js/main.js
+++ Lab5/01-Fawrey-Card-Game/js/main.js
@@ -2,16 +2,14 @@ var botn = document.getElementById("botn");
 var sora = document.getElementById("sora");
 var inpot = document.getElementById("inpot");
 
+function checkthepass() {
+  var answer = inpot.value;
 
-function checkthepass(){
-    var answer = inpot.value;
-
-
-    if(answer == "12345"){
-        sora.classList.remove("myimg")
-sora.style.display = "block";
-    }
-    else{
-        alert("wrong password");
-    }
+  if (answer == "12345") {
+    sora.classList.remove("myimg");
+    sora.style.display = "block";
+    sora2.style.display = "none";
+  } else {
+    alert("wrong password");
+  }
 }
