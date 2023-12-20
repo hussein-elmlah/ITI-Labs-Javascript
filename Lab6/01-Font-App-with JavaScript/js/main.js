e0cf0b1119d8c1f7a661408610aaadaa56f20f69
Hussein Elmlah <147069168+hussein-elmlah@users.noreply.github.com>
2023-12-20 20:23:27 +0200

    Delete Lab6/01-Font-App-with JavaScript directory

--- Lab6/01-Font-App-with JavaScript/js/main.js
+++
@@ -1,9 +0,0 @@
-
-function updateText(classN,fontN){
-    document.getElementById('text2').innerHTML = document.getElementById('text1').value;
-    document.getElementById('text2').className = `${classN}`;
-    document.getElementById('fontName').innerHTML = document.getElementById(`${fontN}`).innerHTML;
-    document.getElementById('fontName').className = `${classN}`;    
-
-}
-
